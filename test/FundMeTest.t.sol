f0531f264bf9949525cdd96d5f4be2cbc790965b
SeanXLChen <xiaolaiseanchen@gmail.com>
2025-01-02 22:24:57 -0800

    Add two tests about `FundMe`

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,10 +2,24 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    function setUp() external {}
+    FundMe fundMe;
 
-    function testDemo() public {}
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    // us -> FundMeTest -> FundMe
+    function testOwnerIsMsgSender() public view {
+        // assertEq(fundMe.i_owner(), msg.sender); 
+        // fails as msg.sender is not the owner, owner of fundMe is FundMeTest
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
