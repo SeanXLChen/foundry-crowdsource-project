03166a9795ba0ed010c113c04574c7fd5a4558ae
SeanXLChen <xiaolaiseanchen@gmail.com>
2025-01-05 09:21:18 -0800

    Add: mainnet config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -21,6 +21,8 @@ contract HelperConfig is Script {
         // sepolia chain id: 11155111
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
         } else {
             activeNetworkConfig = getAnvilEthConfig();
         }
@@ -38,10 +40,26 @@ contract HelperConfig is Script {
 
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
         // return price feed address for anvil eth/usd
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        
+        vm.stopBroadcast();
         NetworkConfig memory anvilConfig = NetworkConfig({
             name: "Anvil",
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
         return anvilConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // return price feed address for mainnet eth/usd
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            name: "Mainnet",
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetConfig;
     }
 }
