12c755cc2bb507d6c7b37cb0402c04815cf67e9c
SeanXLChen <xiaolaiseanchen@gmail.com>
2025-01-05 17:03:09 -0800

    Mod: create a USER to test user interaction with functions

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,10 +9,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant ONE_ETH = 1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE); // send USER some ETH
     }
 
     function testMinimumDollarIsFive() public view {
@@ -30,6 +35,22 @@ contract FundMeTest is Test {
     // `forge test --fork-url $SEPOLIA_RPC_URL`
     function testPriceFeedVersionIsAccurate() public view {
         assertEq(fundMe.getVersion(), 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert("You need to spend more ETH!");
+        fundMe.fund{value: 1e14}(); // 0.0001 ETH
+    }
+
+    function testFundFailsWithoutETH() public {
+        vm.expectRevert("You need to spend more ETH!");
+        fundMe.fund(); // 0.0 ETH
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next tx will be from USER
+        fundMe.fund{value: ONE_ETH}(); // 1 ETH from USER
+        assertEq(fundMe.getAddressToAmountFunded(USER), ONE_ETH);
     }
 
 }
